3e50f9c330ee7c34c9da9ee8f8170ad153af45b2
unknown <mrcplabon@gmail.com>
2025-11-13 20:56:20 +0600

    table cooking

--- src/Components/MyPayBills/MyPayBills.jsx
+++ src/Components/MyPayBills/MyPayBills.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthContext';
 
 const MyPayBills = () => {
+    const {user} = use(AuthContext)
+    const [bills, setBills] = useState([])
+
+    useEffect(()=>{
+        if(user?.email){
+            fetch(`http://localhost:3000/usersBills?email=${user.email}`)
+            .then(res=>res.json())
+            .then(data=>{
+                console.log('User data', data)
+                setBills(data)
+            })
+        }
+    },[user?.email])
+
     return (
         <div>
-            <p>My Pay Bills</p>
+            <p>My Pay Bills : {bills.length}</p>
         </div>
     );
 };
