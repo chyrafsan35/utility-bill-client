f3bfd70b6300583b41d0d8e09fbe046bec9bc632
unknown <mrcplabon@gmail.com>
2025-11-14 20:27:14 +0600

    background for recent bills

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import RecentBills from '../RecentBills/RecentBills';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import tree from '../../assets/tree-branch.png';
 
 const Home = () => {
     const [recentBills, setRecentBills] = useState([]);
@@ -16,12 +17,13 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            
+
             <div className='max-w-[1440px] mx-auto px-5 py-10'>
                 <Category></Category>
             </div>
 
-            <div className='max-w-[1440px] mx-auto px-5 py-10'>
+            <div className=' bg-gradient-to-r from-[#F5DA8C] to-[#EDD180] relative overflow-hidden'>
+                <img className='max-w-[300px] md:max-w-[500px] absolute right-0 top-[-150px]' src={tree} alt="" />
                 <RecentBills recentBills={recentBills}></RecentBills>
             </div>
         </div>
